ef731d066364f01c2b68480553d51028107fe5ab
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-05-17 21:16:46 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,50 @@
+import logging, asyncio
+
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+
+logging.basicConfig(level=logging.ERROR)
+       
+SESSION = environ.get("SESSION", "")        
+User = Client(name="AcceptUser", session_string=SESSION)
+
+
+@User.on_message(filters.command(["run", "approve"], [".", "/"]))                     
+async def approve(client, message):
+    Id = message.chat.id
+    await message.delete(True)
+ 
+    try:
+       while True: # create loop is better techniq to accept within seconds 💀
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        while True:
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+
+    msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
+    await msg.delete()
+
+
+logging.info("Bot Started....")
+User.run()
+
+
+
+
+
+
+
